1aa68eecb13e84a4c44c2cec96afd8e2ba88de43
AlmaJS <alma.bocaj@gmail.com>
2022-06-30 11:55:03 +0100

    created Synonyms.js component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
@@ -8,9 +8,15 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
               <br />
+              <strong>Example:</strong>
+
               <em>{definition.example}</em>
+              <br />
+              <strong>Synonyms:</strong>
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
